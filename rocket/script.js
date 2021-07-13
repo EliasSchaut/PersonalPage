935f1443f381525ee1e198bd1a88fbe9d2d6178f
EliasSchaut <eschaut@outlook.de>
2021-07-14 01:02:46 +0200

    improve code style and element styles

--- rocket/script.js
+++ rocket/script.js
@@ -12,7 +12,7 @@ disable()
 /* ---------------------------------------
 ** Event Listeners
 ** ------------------------------------ */
-window.onload = function () {
+window.onloadeddata = function () {
 	alert("Warning! You enter a critical rocket launch system! \nDont inspect the page!")
 }
 
@@ -21,41 +21,91 @@ ok_button.addEventListener("click", function () {
     const to_check = pass_input.value
 
     if (to_check === password) {
-        pass_input.placeholder = "Correct!"
-        pass_input.setAttribute("style", "border-color: green;")
+        change_placeholder_green("Correct!")
         enable_panel()
-        register_listener()
+        create_listeners()
 
     } else {
-        pass_input.placeholder = "Incorrect!"
-        pass_input.setAttribute("style", "border-color: red;")
-        pass_input.value = ""
+        clear_text_field()
+        change_placeholder_red("Incorrect!")
     }
 })
+
 
 launch.addEventListener("click", function () {
     if (check_max()) {
-        pass_input.value = ""
-        pass_input.placeholder = "LIFTOFF!"
-        pass_input.setAttribute("style", "border-color: green;")
+        clear_text_field()
+        change_placeholder_green("LIFTOFF!")
         liftoff()
 
     } else {
-        pass_input.value = ""
-        pass_input.placeholder = "Not enough energy!"
-        pass_input.setAttribute("style", "border-color: red;")
-
+        clear_text_field()
+        change_placeholder_red("Not enough energy!")
     }
 })
 
-function register_listener() {
+
+function create_listeners() {
     for (const element of document.getElementsByClassName("control-button")) {
-        element.onchange = check_launch
+        element.onchange = check_launch_possible
     }
 
     for (const element of document.getElementsByClassName("control-slider")) {
-        element.onchange = check_launch
+        element.onchange = check_launch_possible
     }
+}
+// -----------------------------------------
+
+
+
+/* ---------------------------------------
+** Enable/Disable
+** ------------------------------------ */
+function enable_panel() {
+    for (const button of document.getElementsByClassName("control-button")) {
+        button.disabled = false
+    }
+
+    for (const slider of document.getElementsByClassName("control-slider")) {
+        slider.disabled = false
+    }
+
+    pass_input.disabled = true
+    ok_button.disabled = true
+    launch.disabled = false
+}
+
+
+function disable() {
+    for (const button of document.getElementsByClassName("control-button")) {
+        button.disabled = true
+    }
+
+    for (const slider of document.getElementsByClassName("control-slider")) {
+        slider.disabled = true
+    }
+
+    launch.disabled = true
+}
+// -----------------------------------------
+
+
+
+/* ---------------------------------------
+** Text Field
+** ------------------------------------ */
+function change_placeholder_green(title = "") {
+    pass_input.placeholder = title
+    pass_input.setAttribute("style", "border-color: green;")
+}
+
+function change_placeholder_red(title = "") {
+    pass_input.placeholder = title
+    pass_input.setAttribute("style", "border-color: red;")
+}
+
+function clear_text_field() {
+    pass_input.value = ""
 }
 // -----------------------------------------
 
@@ -64,40 +114,12 @@ function register_listener() {
 /* ---------------------------------------
 ** Helper
 ** ------------------------------------ */
-function enable_panel() {
-    for (const element of document.getElementsByClassName("control-button")) {
-        element.removeAttribute("disabled")
-    }
-
-    for (const element of document.getElementsByClassName("control-slider")) {
-        element.removeAttribute("disabled")
-    }
-
-    pass_input.setAttribute("disabled", "")
-    ok_button.setAttribute("disabled", "")
-    launch.setAttribute("disabled", "")
-}
-
-
-function disable() {
-    for (const element of document.getElementsByClassName("control-button")) {
-        element.setAttribute("disabled", "")
-    }
-
-    for (const element of document.getElementsByClassName("control-slider")) {
-        element.setAttribute("disabled", "")
-    }
-
-    launch.setAttribute("disabled", "")
-}
-
-
-function check_launch() {
+function check_launch_possible() {
     if (check_max()) {
-        launch.removeAttribute("disabled")
+        clear_text_field()
+        change_placeholder_green("Enough Energy!")
     }
 }
-
 
 function check_max() {
     for (const checkbox of document.getElementsByClassName("control-button")) {
