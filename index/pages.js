8bc14c62bc9ca7535f8d9c8b84a23683f62fa2c2
EliasSchaut <eschaut@outlook.de>
2021-08-25 15:27:35 +0200

    add lib and start editing main page

---
+++ index/pages.js
@@ -0,0 +1,56 @@
+const pages = [
+  {
+    "name": "",
+    "description": "",
+    "img_src": "",
+    "link_to_page": ""
+  },
+  {
+    "name": "",
+    "description": "",
+    "img_src": "",
+    "link_to_page": ""
+  },
+  {
+    "name": "",
+    "description": "",
+    "img_src": "",
+    "link_to_page": ""
+  },
+  {
+    "name": "",
+    "description": "",
+    "img_src": "",
+    "link_to_page": ""
+  },
+  {
+    "name": "",
+    "description": "",
+    "img_src": "",
+    "link_to_page": ""
+  },
+  {
+    "name": "",
+    "description": "",
+    "img_src": "",
+    "link_to_page": ""
+  },
+  {
+    "name": "",
+    "description": "",
+    "img_src": "",
+    "link_to_page": ""
+  },
+  {
+    "name": "",
+    "description": "",
+    "img_src": "",
+    "link_to_page": ""
+  },
+  {
+    "name": "",
+    "description": "",
+    "img_src": "",
+    "link_to_page": ""
+  }
+]
