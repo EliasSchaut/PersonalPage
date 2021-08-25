d1f671733faf0a8e90fc6ed6555aee7ce5d71d85
EliasSchaut <eschaut@outlook.de>
2021-08-25 16:55:50 +0200

    add pictures for almost every of my pages

--- index/pages.js
+++ index/pages.js
@@ -2,64 +2,64 @@ const pages = [
     {
         "title": "About Me",
         "description": "A page that describes me.",
-        "img_src": "",
-        "alt_img": "",
+        "img_src": "index/img/about-me.jpg",
+        "alt_img": "A picture about me",
         "link_to_page": "p/schaut"
     },
     {
         "title": "Games",
         "description": "Links to funny browser games for you and your friends.",
-        "img_src": "",
-        "alt_img": "",
+        "img_src": "index/img/games.png",
+        "alt_img": "A picture of the games page",
         "link_to_page": "p/games"
     },
     {
         "title": "Movie",
         "description": "Enjoy movies with my self created video player.",
-        "img_src": "",
-        "alt_img": "",
+        "img_src": "index/img/movie.png",
+        "alt_img": "A picture of the movie page",
         "link_to_page": "p/movie"
     },
     {
         "title": "Simple Clicker",
         "description": "A simple clicker game.",
-        "img_src": "",
-        "alt_img": "",
+        "img_src": "index/img/clicker.png",
+        "alt_img": "A picture of the clicker",
         "link_to_page": "p/clicker"
     },
     {
         "title": "Epic Rocket Launch",
         "description": "Try to launch the rocket!",
-        "img_src": "",
-        "alt_img": "",
+        "img_src": "index/img/rocket.png",
+        "alt_img": "A picture of rocket page",
         "link_to_page": "p/rocket"
     },
     {
         "title": "Case Converter",
         "description": "A converter that converts your text in the case you want!",
-        "img_src": "",
-        "alt_img": "",
+        "img_src": "index/img/case-converter.png",
+        "alt_img": "A picture of case converter page",
         "link_to_page": "p/case-converter"
     },
     {
         "title": "To Do List",
         "description": "Manage your tasks!",
-        "img_src": "",
-        "alt_img": "",
+        "img_src": "index/img/to-do-list.png",
+        "alt_img": "A picture of the to do list page",
         "link_to_page": "p/to-do-list"
     },
     {
         "title": "Simple Piano",
         "description": "Play the piano!",
-        "img_src": "",
-        "alt_img": "",
+        "img_src": "index/img/piano.png",
+        "alt_img": "A picture of the piano page",
         "link_to_page": "p/piano"
     },
     {
         "title": "Flashcards",
         "description": "This helps you to learn the federal states of germany!",
-        "img_src": "",
-        "alt_img": "",
+        "img_src": "index/img/flashcards.png",
+        "alt_img": "A picture of the flashcards page",
         "link_to_page": "p/flashcards"
     },
     {
