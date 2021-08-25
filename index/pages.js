753560619de643d9df92787f3b11944e11840b2c
EliasSchaut <eschaut@outlook.de>
2021-08-25 16:15:49 +0200

    add description to every page and in pages.js

--- index/pages.js
+++ index/pages.js
@@ -1,77 +1,77 @@
 const pages = [
     {
         "title": "About Me",
-        "description": "",
+        "description": "A page that describes me.",
         "img_src": "",
         "alt_img": "",
         "link_to_page": "p/schaut"
     },
     {
         "title": "Games",
-        "description": "",
+        "description": "Links to funny browser games for you and your friends.",
         "img_src": "",
         "alt_img": "",
         "link_to_page": "p/games"
     },
     {
         "title": "Movie",
-        "description": "",
+        "description": "Enjoy movies with my self created video player.",
         "img_src": "",
         "alt_img": "",
         "link_to_page": "p/movie"
     },
     {
         "title": "Simple Clicker",
-        "description": "",
+        "description": "A simple clicker game.",
         "img_src": "",
         "alt_img": "",
         "link_to_page": "p/clicker"
     },
     {
         "title": "Epic Rocket Launch",
-        "description": "",
+        "description": "Try to launch the rocket!",
         "img_src": "",
         "alt_img": "",
         "link_to_page": "p/rocket"
     },
     {
         "title": "Case Converter",
-        "description": "",
+        "description": "A converter that converts your text in the case you want!",
         "img_src": "",
         "alt_img": "",
         "link_to_page": "p/case-converter"
     },
     {
         "title": "To Do List",
-        "description": "",
+        "description": "Manage your tasks!",
         "img_src": "",
         "alt_img": "",
         "link_to_page": "p/to-do-list"
     },
     {
         "title": "Simple Piano",
-        "description": "",
+        "description": "Play the piano!",
         "img_src": "",
         "alt_img": "",
         "link_to_page": "p/piano"
     },
     {
         "title": "Flashcards",
-        "description": "",
+        "description": "This helps you to learn the federal states of germany!",
         "img_src": "",
         "alt_img": "",
         "link_to_page": "p/flashcards"
     },
     {
-        "title": "Datenschutz",
-        "description": "",
+        "title": "Privacy",
+        "description": "The privacy policy for all my pages.",
         "img_src": "",
         "alt_img": "",
         "link_to_page": "p/datenschutz"
     },
     {
         "title": "Impressum",
-        "description": "",
+        "description": "The site notice for all pages",
         "img_src": "",
         "alt_img": "",
         "link_to_page": "p/impressum"
