80f95a2fa1c2626e61978bb97b7dcac5e46084f2
EliasSchaut <eschaut@outlook.de>
2021-08-25 15:58:22 +0200

    add card creation script

--- index/pages.js
+++ index/pages.js
@@ -1,56 +1,79 @@
 const pages = [
-  {
-    "name": "",
-    "description": "",
-    "img_src": "",
-    "link_to_page": ""
-  },
-  {
-    "name": "",
-    "description": "",
-    "img_src": "",
-    "link_to_page": ""
-  },
-  {
-    "name": "",
-    "description": "",
-    "img_src": "",
-    "link_to_page": ""
-  },
-  {
-    "name": "",
-    "description": "",
-    "img_src": "",
-    "link_to_page": ""
-  },
-  {
-    "name": "",
-    "description": "",
-    "img_src": "",
-    "link_to_page": ""
-  },
-  {
-    "name": "",
-    "description": "",
-    "img_src": "",
-    "link_to_page": ""
-  },
-  {
-    "name": "",
-    "description": "",
-    "img_src": "",
-    "link_to_page": ""
-  },
-  {
-    "name": "",
-    "description": "",
-    "img_src": "",
-    "link_to_page": ""
-  },
-  {
-    "name": "",
-    "description": "",
-    "img_src": "",
-    "link_to_page": ""
-  }
+    {
+        "title": "About Me",
+        "description": "",
+        "img_src": "",
+        "alt_img": "",
+        "link_to_page": "p/schaut"
+    },
+    {
+        "title": "Games",
+        "description": "",
+        "img_src": "",
+        "alt_img": "",
+        "link_to_page": "p/games"
+    },
+    {
+        "title": "Movie",
+        "description": "",
+        "img_src": "",
+        "alt_img": "",
+        "link_to_page": "p/movie"
+    },
+    {
+        "title": "Simple Clicker",
+        "description": "",
+        "img_src": "",
+        "alt_img": "",
+        "link_to_page": "p/clicker"
+    },
+    {
+        "title": "Epic Rocket Launch",
+        "description": "",
+        "img_src": "",
+        "alt_img": "",
+        "link_to_page": "p/rocket"
+    },
+    {
+        "title": "Case Converter",
+        "description": "",
+        "img_src": "",
+        "alt_img": "",
+        "link_to_page": "p/case-converter"
+    },
+    {
+        "title": "To Do List",
+        "description": "",
+        "img_src": "",
+        "alt_img": "",
+        "link_to_page": "p/to-do-list"
+    },
+    {
+        "title": "Simple Piano",
+        "description": "",
+        "img_src": "",
+        "alt_img": "",
+        "link_to_page": "p/piano"
+    },
+    {
+        "title": "Flashcards",
+        "description": "",
+        "img_src": "",
+        "alt_img": "",
+        "link_to_page": "p/flashcards"
+    },
+    {
+        "title": "Datenschutz",
+        "description": "",
+        "img_src": "",
+        "alt_img": "",
+        "link_to_page": "p/datenschutz"
+    },
+    {
+        "title": "Impressum",
+        "description": "",
+        "img_src": "",
+        "alt_img": "",
+        "link_to_page": "p/impressum"
+    }
 ]
--- index/script.js
+++ index/script.js
@@ -1,24 +1,50 @@
-/**
- <div class="card" style="width: 18rem;">
-    <img src="" class="card-img-top" alt="...">
-    <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-    </div>
- </div>
- */
+
+const div_pages = document.getElementById("pages")
+const page_link_label = "Go to page"
+insert_cards()
 
 function insert_cards() {
-
     for (const page of pages) {
-        generate_card(page)
+        div_pages.appendChild(generate_card(page))
     }
 }
 
 
 function generate_card(page) {
 
+    // create tags
+    const card = document.createElement("div")
+    const img = document.createElement("img")
+    const body = document.createElement("div")
+    const title = document.createElement("h5")
+    const description = document.createElement("p")
+    const page_link = document.createElement("a")
+
+    // add attributes
+    card.setAttribute("class", "card")
+    card.setAttribute("style", "width: 18rem;")
+    img.setAttribute("src", page.img_src)
+    img.setAttribute("class", "card-img-top")
+    img.setAttribute("alt", page.alt_img)
+    body.setAttribute("class", "card-body")
+    title.setAttribute("class", "card-title")
+    description.setAttribute("class", "card-text")
+    page_link.setAttribute("href", page.link_to_page)
+    page_link.setAttribute("class", "btn btn-primary")
+
+    // insert inner html
+    title.innerHTML = page.title
+    description.innerHTML = page.description
+    page_link.innerHTML = page_link_label
+
+    // append children
+    body.appendChild(title)
+    body.appendChild(description)
+    body.appendChild(page_link)
+    card.appendChild(img)
+    card.appendChild(body)
+
+    return card
 }
 
 
