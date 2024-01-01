5f7519ba2e3dddf568fbcd2a18d21ad0937cf9a8
EliasSchaut <eschaut@outlook.de>
2024-01-01 21:10:12 +0100

    add content feature to add blog like pages with markdown syntax + refactoring + nav for mobile

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,6 +1,9 @@
 <template>
   <Body class="bg-white dark:bg-gray-800" />
   <div class="bg-white dark:bg-gray-900 dark:text-white">
+    <div class="flex justify-center">
+      <LayoutNav />
+    </div>
     <LayoutBanner />
     <slot />
     <LayoutFooter />
