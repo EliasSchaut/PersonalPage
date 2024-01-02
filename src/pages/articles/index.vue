b5346012efc29b88b6eaeeffeb79c24115d8fd40
EliasSchaut <eschaut@outlook.de>
2024-01-02 03:27:50 +0100

    Replace ContentNavigation with ContentList in articles index for better performance

--- src/pages/articles/index.vue
+++ src/pages/articles/index.vue
@@ -4,15 +4,11 @@
     intro="Alle meine Ideen, Erfahrungen, Umsetzungen und mehr gesammelt in chronologischer Reihenfolge."
   >
     <nav>
-      <ContentNavigation v-slot="{ navigation }">
+      <ContentList path="/articles" v-slot="{ list }">
         <ul
           class="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3 dark:border-gray-500"
         >
-          <li
-            v-for="article of navigation.find((e) => e.title === 'Articles')
-              .children"
-            :key="article._path"
-          >
+          <li v-for="article of list" :key="article._path">
             <ArticlePreview
               :title="article.title"
               :href="article._path"
@@ -23,7 +19,7 @@
             />
           </li>
         </ul>
-      </ContentNavigation>
+      </ContentList>
     </nav>
   </LayoutHeader>
 </template>
