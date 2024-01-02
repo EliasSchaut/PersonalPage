9d9ac586e8d848be91d87225e47baa4f1c958c75
EliasSchaut <eschaut@outlook.de>
2024-01-02 23:53:27 +0100

    Add resources page and typography styles
    Added a new resources page and several vue components to support it. Also included typography styles using Tailwind, which involves an update to both package.json and tailwind.config.ts. Improved layout and navigation features and added ProseA vue component for link styling.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,23 @@
 import type { Config } from 'tailwindcss';
 
+import typography_styles from './typography';
+
 export default <Partial<Config>>{
-    darkMode: 'class',
-    content: [
-        './components/**/*.{js,vue,ts}',
-        './layouts/**/*.vue',
-        './pages/**/*.vue',
-        './plugins/**/*.{js,ts}',
-        './nuxt.config.{js,ts}',
-        './app.vue',
-    ],
-    theme: {
-        extend: {},
-    },
-    plugins: [require('@tailwindcss/forms')],
+  darkMode: 'class',
+  content: [
+    './components/**/*.{js,vue,ts}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './nuxt.config.{js,ts}',
+    './app.vue',
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+  typography: typography_styles,
 };
