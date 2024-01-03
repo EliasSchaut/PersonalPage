4bb753d7564b64c4f5d4e74def0d367d97cc133d
EliasSchaut <eschaut@outlook.de>
2024-01-03 01:40:19 +0100

    Replace "Hello World" article with "Bday" articles and update badge component
    The "Hello World" article has been removed and replaced with two "Bday" articles concerning the author's 22nd and 23rd birthdays. In addition, the badge component was refactored, introducing a new simple version and renaming the main file to index.vue. The simple version can accept an optional href prop.

---
+++ src/components/badge/simple.vue
@@ -0,0 +1,33 @@
+<template>
+  <a
+    v-if="href !== '#'"
+    class="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700"
+    :href="href"
+    target="_blank"
+  >
+    {{ content }}
+  </a>
+  <span
+    v-else
+    class="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 dark:bg-gray-800 dark:text-gray-400"
+  >
+    {{ content }}
+  </span>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+    content: {
+      type: String,
+      required: true,
+    },
+    href: {
+      type: String,
+      default: '#',
+    },
+  },
+});
+</script>
