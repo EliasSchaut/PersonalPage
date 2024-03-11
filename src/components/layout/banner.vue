40bf435712a47956a7e868b728099bac2e085472
EliasSchaut <eschaut@outlook.de>
2024-03-11 12:21:58 +0100

    update email

--- src/components/layout/banner.vue
+++ src/components/layout/banner.vue
@@ -113,7 +113,7 @@ export default defineComponent({
   setup() {
     return {
       name: 'Elias Schaut',
-      email: 'eschaut@outlook.de',
+      email: 'elias@schaut.dev',
       backgroundImage:
         'https://images.unsplash.com/photo-1444628838545-ac4016a5418a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80',
       donate: [
