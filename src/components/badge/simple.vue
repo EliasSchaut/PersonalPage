0b94a044a14b1978f5564caa9fc3283649d8acd5
Elias <elias@schaut.dev>
2025-01-22 17:27:07 +0000

    Add bday24 cover picture

--- src/components/badge/simple.vue
+++ src/components/badge/simple.vue
@@ -7,13 +7,13 @@
   >
     {{ content }}
   </a>
-  <nuxt-link
+  <NuxtLink
     v-else-if="href !== '#'"
     class="relative z-10 rounded-full bg-second-200 px-3 py-1.5 font-medium text-second-600 hover:bg-second-300 dark:bg-second-800 dark:text-second-400 dark:hover:bg-second-700"
     :href="href"
   >
     {{ content }}
-  </nuxt-link>
+  </NuxtLink>
   <span
     v-else
     class="relative z-10 rounded-full bg-second-50 px-3 py-1.5 font-medium text-second-600 dark:bg-second-800 dark:text-second-400"
