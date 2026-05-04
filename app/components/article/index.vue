d9d64923f57354494c33ec62440f49a2c1d3cc91
Elias Schaut <eschaut@outlook.de>
2026-05-04 22:08:43 +0200

    fix: correct image paths and improve article component styling

--- app/components/article/index.vue
+++ app/components/article/index.vue
@@ -1,6 +1,6 @@
 <template>
   <article
-    class="prose mx-auto w-full px-6 py-24 text-base leading-7 text-second-700 dark:prose-invert lg:prose-xl dark:text-second-100"
+    class="prose max-w-none text-second-700 dark:prose-invert lg:prose-xl dark:text-second-100"
   >
     <slot />
   </article>
